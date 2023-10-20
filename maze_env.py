2536bdc2467635f19587c1715186b560ee8aa3e9
Marcus <ghoshm@hotmail.co.uk>
2023-10-20 17:02:20 +0100

    Improved implementation with padding for the mazes

--- maze_env.py
+++ maze_env.py
@@ -4,22 +4,18 @@ import numpy as np
 
 
 class maze:
-    def __init__(self, size, n_channels, goal_grad, avoid_grad):
+    def __init__(self, size, n_channels):
         """
         Maze object.
         Arguments:
             size: the size of the square maze [n]
             n_channels: the number of input channels [n]
-            goal_grad:
-            avoid_grad:
         """
-
         self.size = size
         self.n_channels = n_channels
-        # self.goal_grad = goal_grad
-        # self.avoid_grad = avoid_grad
         self.mazes = []
-        self.goals = []
+        self.goal_locations = []
+        self.goal_channels = []
         self.maze_type = []
 
     def generate_track_mazes(self, number):
@@ -32,43 +28,45 @@ class maze:
             is a np array of size x size x channels + 1.
             Where [:,:,-1] stores the maze structure.
         """
-        # Set goals as left (-1) or right (1)
-        goal_locations = np.repeat([0, self.size - 1], repeats=number // 2)
+        # Set goal locations as left (1) or right (size - 2)
+        goal_locations = np.repeat([1, self.size - 2], repeats=number // 2)
 
         # Set which channel encodes the goal
         goal_channels = np.repeat([0, 1], repeats=number // 2)
         np.random.shuffle(goal_channels)
 
         mazes = []
-        indices = np.arange(self.size)
         for n in range(number):
-            # Generate track (0.) and walls  (1.)
             maze = np.zeros(
                 shape=(self.size, self.size, self.n_channels + 1), dtype="double"
             )
-            maze[indices != self.size // 2, :, -1] = 1.0
+
+            # Generate track (0.) and walls  (1.)
+            maze[:, :, -1] = 1.0
+            maze[self.size // 2, 1:-1, -1] = 0.0
 
             # Set up gradients
-            gradient = np.linspace(start=0.1, stop=1.0, num=((self.size - 1) // 2))
+            gradient = np.linspace(start=0.1, stop=1.0, num=((self.size - 2) // 2))
 
             # Misleading channel
-            maze[self.size // 2, :, (1 - goal_channels[n])] = np.concatenate(
+            maze[self.size // 2, 1:-1, (1 - goal_channels[n])] = np.concatenate(
                 (gradient[::-1], np.array(0)[None], gradient)
             )
 
             # Leading channel
-            if goal_locations[n] == 0:
+            if goal_locations[n] == 1:
                 maze[
-                    self.size // 2, 0 : (self.size - 1) // 2, goal_channels[n]
+                    self.size // 2, 1 : (self.size - 1) // 2, goal_channels[n]
                 ] = gradient[::-1]
             else:
                 maze[
-                    self.size // 2, (self.size + 1) // 2 :, goal_channels[n]
+                    self.size // 2, (self.size + 1) // 2 : -1, goal_channels[n]
                 ] = gradient
 
             # Append to list
             mazes.append(maze)
 
         self.mazes = mazes
-        self.goals = goal_locations
+        self.goal_locations = goal_locations
+        self.goal_channels = goal_channels
         self.maze_type = "Track"
--- maze_exp.py
+++ maze_exp.py
@@ -8,14 +8,20 @@ from maze_env import *
 
 
 def eval_fitness(genome, config, n_steps=50):
-    """ """
-    agnt = agent(location=[4, 4], channels=[0, 1], genome=genome, config=config)
-
+    """
+    Evalutes the fitness of the provided genome.
+    Arguments:
+        genome: neat generated genome.
+        config: the neat configuration holder.
+        n_steps: number of simulation steps.
+    Returns:
+        fitness: the mean fitness across mazes, between [0,1].
+    """
     fitness = []
+    fastest_solution = ((track.size - 2) // 2) - 1
     # For each maze
     for mz_n, mz in enumerate(track.mazes):
-        # Reset the agents location
-        agnt.location = [4, 4]
+        agnt = agent(location=[5, 5], channels=[1, 1], genome=genome, config=config)
 
         # Sensation-action loop
         for t in range(n_steps):
@@ -23,27 +29,43 @@ def eval_fitness(genome, config, n_steps=50):
             agnt.act(mz)
 
             # If the end is reached
-            if agnt.location[1] == track.goals[mz_n]:
+            if agnt.location[1] == track.goal_locations[mz_n]:
                 break
 
         # Record fitness
         fitness.append(t)
 
+    # ADD: store simple info on each agent (see book, maze_exp.py line 72-83)
+
     # Normalise fitness
-    fitness = 1 - ((np.array(fitness).mean() - 3) / 46)
+    fitness = 1 - (
+        (np.array(fitness).mean() - fastest_solution) / (n_steps - 1 - fastest_solution)
+    )
 
     # Return fitness
     return fitness
 
 
 def eval_genomes(genomes, config):
-    """ """
+    """
+    Evaluates the fitness of each genome in the population.
+    Arguments:
+        genomes: the list of genomes in the current population.
+        config: the neat configuration holder.
+    Updates:
+        genome.fitness
+    """
     for _, genome in genomes:
         genome.fitness = eval_fitness(genome, config)
 
 
 def run(config_file, n_generations):
-    """ """
+    """
+    Runs the experiment.
+    Arguments:
+        config_file: the path to the config.ini file.
+        n_generations: the number of generations to run.
+    """
     # Load config
     config = neat.Config(
         neat.DefaultGenome,
@@ -66,6 +88,8 @@ def run(config_file, n_generations):
 
     # Output
     print(best_genome.fitness)
+    print(best_genome.size())
+    print(best_genome.connections)
 
 
 if __name__ == "__main__":
@@ -73,7 +97,7 @@ if __name__ == "__main__":
     n_generations = 50
 
     # Generate mazes
-    track = maze(size=9, n_channels=2, goal_grad=0, avoid_grad=0)
+    track = maze(size=11, n_channels=2)
     track.generate_track_mazes(50)
 
     # Run
